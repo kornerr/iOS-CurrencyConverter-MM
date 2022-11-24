14f04118b43fc1a5853f7bf24f7964ca03ab762c
Михаил Капелько <kornerr@gmail.com>
2022-11-24 15:23:52 +0300

    d

--- mod/Converter/src/Converter.Core.swift
+++ mod/Converter/src/Converter.Core.swift
@@ -41,22 +41,18 @@ extension Converter {
         selectedCurrencySrcId: vm.$selectedCurrencySrcId.eraseToAnyPublisher(),
         showInfo: vm.showInfo.eraseToAnyPublisher()
       )
-
-      setupUI()
-      setupNetwork()
-      setupStorage()
+      setupReactions(ctrl)
       showUI()
-
       ctrl.start()
-
     }
   }
 }
 
 extension Converter.Core {
-  private func setupNetwork() {
+  private func setupReactions(_ ctrl: Converter.Controller) {
     // Загружаем курсы валют.
-    m.compactMap { $0.shouldRefreshExchangeRates }
+    ctrl.m
+      .compactMap { $0.shouldRefreshExchangeRates }
       /**/.handleEvents(receiveOutput: { _ in print("ИГР ConverterC.setupN shouldRER-1") })
       .flatMap { [weak self] url -> AnyPublisher<Net.ExchangeRates?, Never> in
         self?.hasStartedUpdatingExchangeRates.send()
@@ -66,122 +62,129 @@ extension Converter.Core {
       /**/.handleEvents(receiveOutput: { o in print("ИГР ConverterC.setupN shouldRER-2: '\(o)'") })
       .sink { [weak self] v in self?.resultExchangeRates.send(v) }
       .store(in: &subscriptions)
-  }
 
-  private func setupUI() {
     // Форматируем поле ввода.
-    m.compactMap { $0.shouldResetAmountSrc }
+    ctrl.m
+      .compactMap { $0.shouldResetAmountSrc }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.amountSrc = v }
       .store(in: &subscriptions)
 
     // Вычисляем результат.
-    m.compactMap { $0.shouldResetAmountDst }
+    ctrl.m
+      .compactMap { $0.shouldResetAmountDst }
       .receive(on: DispatchQueue.main)
       /**/.handleEvents(receiveOutput: { o in print("ИГР ConverterC.setupU shouldRAD: '\(o)'") })
       .sink { [weak self] v in self?.vm.amountDst = v }
       .store(in: &subscriptions)
 
     // Задаём список валют.
-    m.compactMap { $0.shouldResetCurrencies }
+    ctrl.m
+      .compactMap { $0.shouldResetCurrencies }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.currencies = v }
       .store(in: &subscriptions)
 
     // Задаём валюту-источник.
-    m.compactMap { $0.shouldResetCurrencySrc }
+    ctrl.m
+      .compactMap { $0.shouldResetCurrencySrc }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.currencySrc = v }
       .store(in: &subscriptions)
 
     // Задаём первоначальную установку валюты-источника.
-    m.compactMap { $0.shouldResetCurrencySrcId }
+    ctrl.m
+      .compactMap { $0.shouldResetCurrencySrcId }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.selectedCurrencySrcId = v }
       .store(in: &subscriptions)
 
     // Задаём валюту-назначение.
-    m.compactMap { $0.shouldResetCurrencyDst }
+    ctrl.m
+      .compactMap { $0.shouldResetCurrencyDst }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.currencyDst = v }
       .store(in: &subscriptions)
 
     // Задаём первоначальную установку валюты-назначения.
-    m.compactMap { $0.shouldResetCurrencyDstId }
+    ctrl.m
+      .compactMap { $0.shouldResetCurrencyDstId }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.selectedCurrencyDstId = v }
       .store(in: &subscriptions)
 
     // Сообщаем об ошибке.
-    m.compactMap { $0.shouldReportError }
+    ctrl.m
+      .compactMap { $0.shouldReportError }
       .receive(on: DispatchQueue.main)
       .sink { Self.reportError($0) }
       .store(in: &subscriptions)
 
     // Задаём видимость списка валют для источника.
-    m.compactMap { $0.shouldResetPickerSrcVisibility }
+    ctrl.m
+      .compactMap { $0.shouldResetPickerSrcVisibility }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.isPickerSrcVisible = v }
       .store(in: &subscriptions)
 
     // Задаём видимость списка валют для назначения.
-    m.compactMap { $0.shouldResetPickerDstVisibility }
+    ctrl.m
+      .compactMap { $0.shouldResetPickerDstVisibility }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.isPickerDstVisible = v }
       .store(in: &subscriptions)
 
     // Задаём справочный курс единицы валюты.
-    m.compactMap { $0.shouldResetSingleRate }
+    ctrl.m
+      .compactMap { $0.shouldResetSingleRate }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.rate = v }
       .store(in: &subscriptions)
 
     // Задаём дату обновления курса.
-    m.compactMap { $0.shouldResetRatesDate }
+    ctrl.m
+      .compactMap { $0.shouldResetRatesDate }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.ratesDate = v }
       .store(in: &subscriptions)
 
     // Задаём статус актуальности курса.
-    m.compactMap { $0.shouldResetRatesStatus }
+    ctrl.m
+      .compactMap { $0.shouldResetRatesStatus }
       .receive(on: DispatchQueue.main)
       .sink { [weak self] v in self?.vm.areRatesUpToDate = v }
       .store(in: &subscriptions)
 
     // Задаём статус загрузки курса.
-    m.compactMap { $0.shouldResetRatesUpdate }
+    ctrl.m
+      .compactMap { $0.shouldResetRatesUpdate }
       .throttle(for: 0.5, scheduler: DispatchQueue.main, latest: true)
       .sink { [weak self] v in self?.vm.isUpdatingRates = v }
       .store(in: &subscriptions)
-  }
-}
 
-// MARK: - Storage
-
-extension Converter.Core {
-  private func setupStorage() {
     // Сохраняем текущее состояние приложения на диск.
-    m.compactMap { $0.shouldResetDiskState }
+    ctrl.m
+      .compactMap { $0.shouldResetDiskState }
       .receive(on: DispatchQueue.main)
       .sink { state in Disk.saveState(state) }
       .store(in: &subscriptions)
   }
 }
 
-extension Converter.Core {
-  private static func reportError(_ msg: String) {
-    let alert = Alert.VC(title: msg, message: nil, preferredStyle: .alert)
-    let ok = UIAlertAction(title: "OK", style: .default)
-    alert.addAction(ok)
-    alert.show()
-  }
-}
+// MARK: - Вспомогательное
 
 extension Converter.Core {
   private func hideUI() {
     wnd?.rootViewController?.presentedViewController?.dismiss(animated: true) { [weak self] in
       self?.wnd = nil
     }
+  }
+
+  private static func reportError(_ msg: String) {
+    let alert = Alert.VC(title: msg, message: nil, preferredStyle: .alert)
+    let ok = UIAlertAction(title: "OK", style: .default)
+    alert.addAction(ok)
+    alert.show()
   }
 
   private func showUI() {
